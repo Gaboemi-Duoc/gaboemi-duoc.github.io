d084e1cfaad640fccd5aff81e09702d69054946f
Gabo-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2025-10-23 12:50:10 +0000

    Ahora si, creo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  rules: {
+    "no-unused-vars": "off", // Disable the base ESLint rule
+    "@typescript-eslint/no-unused-vars": "off", // Disable the TypeScript specific rule
+  },
+};
 
 export default eslintConfig;
